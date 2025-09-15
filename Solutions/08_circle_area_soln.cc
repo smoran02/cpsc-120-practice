55f1f96062ccee8d70d2a96c3b24d5532cdd9ee2
Spencer Moran <smoran02@gmail.com>
2025-09-14 21:20:48 -0700

    Fixed variable name style

--- Solutions/08_circle_area_soln.cc
+++ Solutions/08_circle_area_soln.cc
@@ -8,13 +8,13 @@ int main() {
 
 	int radius{};
 	// Use a constant variable prefixed with k to store PI
-	const double k_pi{3.14};
+	const double kPi{3.14};
 
 	std::cout << "Enter the radius: ";
 	std::cin >> radius;
 
 	//Cacluate the area of the circle using pi*r^2
-	double area{radius * radius * k_pi};
+	double area{radius * radius * kPi};
 
 	std::cout << "Area of the circle: " << area << "\n";
 
